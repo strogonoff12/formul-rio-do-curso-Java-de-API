978484a6bfeaf43a938a9d3a6394b3c1189c80cd
Gabriel <128945141+strogonoff12@users.noreply.github.com>
2023-08-30 21:43:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("mandando oi para um amigo");
+
+function mandamensagem() {
+    console.log("tudo bem?");
+    console.log("vou mandar uma solicitação");
+    console.log("solicitação recebida");
+}
+
+mandamensagem();
+
+console.log("tchau");
